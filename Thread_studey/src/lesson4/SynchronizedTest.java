f59ea4c2dfdd318568ae3b3cbfab822314ad3cb5
zz070 <2389655715@qq.com>
2020-05-19 21:14:11 +0800

    Synchronized关键字练习

--- Thread_studey/src/lesson4/SynchronizedTest.java
+++ Thread_studey/src/lesson4/SynchronizedTest.java
@@ -19,4 +19,31 @@ public class SynchronizedTest {
             Thread.sleep(99999999);
         }
     }
+
+    public static void main(String[] args) {
+        //test2在不同线程中是否会同步互斥
+        //情况1，不会同步互斥
+        new Thread(() -> {
+            try {
+                new SynchronizedTest().test2();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                new SynchronizedTest().test2();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        //情况2，会同步互斥
+        SynchronizedDemo t =new SynchronizedDemo();
+        new Thread(() -> {
+            t.test2();
+        }).start();
+        new Thread(() -> {
+            t.test2();
+        }).start();
+    }
 }
