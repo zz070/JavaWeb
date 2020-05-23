4c5a683911877cbabb593f318db0cc28208d1a71
zz070 <2389655715@qq.com>
2020-05-23 17:52:34 +0800

    加上volatile的双重校验锁单例模式

---
+++ Thread_studey/src/lesson6/VolatileSingle.java
@@ -0,0 +1,29 @@
+package lesson6;
+
+import lesson5.SafeSington;
+
+public class VolatileSingle {
+    //双重校验锁的单例模式
+    public volatile static SafeSington SAFESINGTON;
+    //volatile的使用保证了变量使用前后的顺序性
+    public static SafeSington getInstance(){
+        //多个线程进入代码块，按序执行
+        if(SAFESINGTON == null){
+            synchronized (SafeSington.class){
+                //第一个进入的线程初始化对象，后序进入的直接退出
+                //后续的线程并发的执行getInstance(),不会进入if代码，直接返回已实例的对象
+                //效率非常高
+                if(SAFESINGTON == null){
+                    SAFESINGTON = new SafeSington();
+                    /*
+                    SAFESINGTON = new SafeSington()分解为
+                    （1）分配对象的内存空间
+                    （2）初始化对象
+                    （3）对象赋值给变量
+                     */
+                }
+            }
+        }
+        return SAFESINGTON;
+    }
+}
