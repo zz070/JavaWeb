84423cad98f37768696c760acd42309d2e752254
zz070 <2389655715@qq.com>
2020-05-25 22:47:50 +0800

    first

--- Thread_studey/src/lesson1/First.java
+++ Thread_studey/src/lesson1/First.java
@@ -1,7 +1,7 @@
 package lesson1;
 
 public class First {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(99999999999L);
     }
 }
