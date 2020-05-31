bfbbef3e740253e9abbb6ed5d39dfc333d6c077b
zz070 <2389655715@qq.com>
2020-05-31 13:39:24 +0800

    PrintWriteTest

--- IO_study/src/PrintWriteTest.java
+++ IO_study/src/PrintWriteTest.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class PrintWriteTest {
     @Test
     public void t1(){
@@ -8,7 +11,12 @@ public class PrintWriteTest {
             int i =  1/0;
             System.out.println("1/0");
         } catch (Exception e) {
-            System.out.println("error");
+           // System.out.println("error");
+           // e.printStackTrace();
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            System.out.println(sw.toString());
         }
         System.out.println("t1");
     }
